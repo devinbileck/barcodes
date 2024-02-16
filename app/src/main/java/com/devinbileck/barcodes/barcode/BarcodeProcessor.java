5a0e2bdd98d9809128550e049394378cf6ca12d9
Devin Bileck <603793+devinbileck@users.noreply.github.com>
2024-02-15 23:05:34 -0800

    fix: BarcodeProcessor failing to process test QR images
    In zxing versions after 3.5.0, disabling the PURE_BARCODE hint seems
    necessary now.

--- app/src/main/java/com/devinbileck/barcodes/barcode/BarcodeProcessor.java
+++ app/src/main/java/com/devinbileck/barcodes/barcode/BarcodeProcessor.java
@@ -1,29 +1,28 @@
 package com.devinbileck.barcodes.barcode;
 
-import java.awt.image.BufferedImage;
-import java.util.Optional;
-
-import javax.validation.constraints.NotNull;
-
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.LuminanceSource;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.Result;
+import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
+
+import javax.validation.constraints.NotNull;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+import java.util.Optional;
 
 public class BarcodeProcessor {
     private BarcodeProcessor() {
     }
 
     public static Optional<BarcodeResult> process(final @NotNull BufferedImage image) {
-        LuminanceSource source = new BufferedImageLuminanceSource(image);
-        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        final LuminanceSource source = new BufferedImageLuminanceSource(image);
+        final BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        final Map<DecodeHintType, Object> hints = Map.of(
+                DecodeHintType.PURE_BARCODE, Boolean.FALSE
+        );
 
-        BarcodeResult result;
+        final BarcodeResult result;
         try {
-            Result r = new MultiFormatReader().decode(bitmap);
+            final Result r = new MultiFormatReader().decode(bitmap, hints);
             result = new BarcodeResult(r.getBarcodeFormat(), r.getText(), image);
         } catch (NotFoundException ignored) {
             // There is no barcode in the image
